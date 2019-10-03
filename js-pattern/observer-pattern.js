8a8935e0f3e63c6e9d923b37123e4892514bee3f
itsaiub <itsaiub@gmail.com>
2019-10-03 22:14:58 +0600

    convert observer pattern to ES6

--- js-pattern/observer-pattern.js
+++ js-pattern/observer-pattern.js
@@ -1,33 +1,56 @@
-function EventObserver() {
-	this.observers = [];
-}
+// function EventObserver() {
+// 	this.observers = [];
+// }
 
-EventObserver.prototype = {
-	subscribe: function(fn) {
+// EventObserver.prototype = {
+// 	subscribe: function(fn) {
+// 		this.observers.push(fn);
+// 		console.log(`You are now subscribed to ${fn.name}`);
+// 	},
+
+// 	unsubscribe: function(fn) {
+// 		/*
+//    Filter out from the list whatever matches the callback function. If
+//    there is no match, the callback gets to stay on the list. The filter
+//    returns a new list and reassigns the list of observers.
+//   */
+// 		this.observers = this.observers.filter(function(item) {
+// 			if (item !== fn) {
+// 				return item;
+// 			}
+// 		});
+// 		console.log(`You are now unsubscribe from ${fn.name}`);
+// 	},
+
+// 	fire: function() {
+// 		this.observers.forEach(function(item) {
+// 			item.call();
+// 		});
+// 	}
+// };
+
+class EventObserver {
+	constructor() {
+		this.observers = [];
+	}
+	subscribe(fn) {
 		this.observers.push(fn);
 		console.log(`You are now subscribed to ${fn.name}`);
-	},
-
-	unsubscribe: function(fn) {
-		/*
-   Filter out from the list whatever matches the callback function. If
-   there is no match, the callback gets to stay on the list. The filter 
-   returns a new list and reassigns the list of observers.
-  */
-		this.observers = this.observers.filter(function(item) {
+	}
+	unsubscribe(fn) {
+		this.observers = this.observers.filter((item) => {
 			if (item !== fn) {
 				return item;
 			}
 		});
-		console.log(`You are now unsubscribe from ${fn.name}`);
-	},
-
-	fire: function() {
-		this.observers.forEach(function(item) {
+		console.log(`You are now unsubsribed to ${fn.name}`);
+	}
+	fire(fn) {
+		this.observers.forEach((item) => {
 			item.call();
 		});
 	}
-};
+}
 
 const click = new EventObserver();
 // Event listeners
